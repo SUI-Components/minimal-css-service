969dbead896a27197b177bc3ed91366f7a371903
midudev <miduga@gmail.com>
2019-12-19 16:05:51 +0100

    Simulate req.query object

--- dev.js
+++ dev.js
@@ -1,6 +1,12 @@
 const http = require('http')
+const url = require('url')
 const extractCssFromUrl = require('.')
 
-http.createServer(extractCssFromUrl).listen(1337, () => {
-  console.log(`Server started at http://localhost:1337`)
-})
+http
+  .createServer((req, res) => {
+    req.query = url.parse(req.url, true).query
+    return extractCssFromUrl(req, res)
+  })
+  .listen(1337, () => {
+    console.log(`Server started at http://localhost:1337`)
+  })
